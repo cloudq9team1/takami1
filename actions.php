ed930fc169923a705d9fd02ed5ad0d77edcd9877
Makoto Takami <cloudq9team1@gmail.com>
2014-06-09 14:11:28 +0900

    mongo port number changed

--- actions.php
+++ actions.php
@@ -1,5 +1,5 @@
 <?php
-$m = new MongoClient( 'mongodb://localhost:12345');
+$m = new MongoClient( 'mongodb://localhost:17423');
 $cursor = $m->tem->users->find( array( 'name' => 'marat'));
 $result = $cursor->getNext();
 echo json_encode( $result); 
